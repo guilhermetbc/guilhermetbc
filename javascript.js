ae15808ef7f23ac8a5ffee189b57954935db8463
guilhermetbc <31928509+guilhermetbc@users.noreply.github.com>
2018-10-22 00:50:16 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
 window.location = "#projeto1";
+
+window.onload = function(){
+  var entidade = document.getElementById('imagem');
+
+  // Altere o número para a apliação/redução desejada
+  var fator_lupa = 6;
+
+  entidade.onmouseover = function () { this.style.width = (this.clientWidth * fator_lupa) + "px"; };
+
+  entidade.onmouseout = function () { this.style.width = (this.clientWidth / 2) + "px"; };
+}
